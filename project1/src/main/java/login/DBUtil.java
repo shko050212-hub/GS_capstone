e3d578031b2e063c7858540522ee153742d9f94f
shko05012-hub <shko050212@gmail.com>
2026-04-15 11:49:04 +0900

    Fix: Update DB connection info in DBUtil

--- project1/src/main/java/login/DBUtil.java
+++ project1/src/main/java/login/DBUtil.java
@@ -6,21 +6,21 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL =
-        "jdbc:mysql://localhost:3306/login_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
+   private static final String URL =
+   "jdbc:mysql://capstone-db.cfw4cygk4dks.ap-northeast-2.rds.amazonaws.com:3306/login_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
+ 
+    private static final String USER = "admin";
+    private static final String PASSWORD = "12341234";
 
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234";
-
-    public static Connection getConnection() throws SQLException {
+    static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new SQLException("드라이버 로드 실패", e);
+            throw new RuntimeException("MySQL 드라이버 로드 실패", e);
         }
+    }
 
-        System.out.println("현재 DB URL: " + URL);
-
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
